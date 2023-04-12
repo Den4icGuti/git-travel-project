a8b23d4b63bec070691bdcde6fec62c32ed27c2f
Den4icGuti <goos7412@gmail.com>
2023-04-12 15:42:45 +0300

    add lib toast

--- js/index.js
+++ js/index.js
@@ -1,24 +1,9 @@
-// import Toastify from "toastify-js";
-// import "toastify-js/src/toastify.css";
-
+import toast from "./service";
 const refs = {
   btnOpenForm: document.querySelector(".js-btn-form"),
   formGroup: document.querySelector(".js-form-group"),
   form: document.querySelector(".js-sign-form"),
 };
-
-// const toast = Toastify({
-//   text: "All fields must be hidden",
-//   duration: 3000,
-//   newWindow: true,
-//   close: true,
-//   gravity: "top", // `top` or `bottom`
-//   position: "center", // `left`, `center` or `right`
-//   stopOnFocus: true, // Prevents dismissing of toast on hover
-//   style: {
-//     background: "#b1693d",
-//   },
-// });
 
 console.log(refs);
 
@@ -54,7 +39,7 @@ function onSubmitForm(e) {
   const password = e.currentTarget.password.value;
 
   if (email === "" || password.length < 4) {
-    alert("All fields must be hidden");
+    toast.showToast("All fields must be filled");
     return;
   }
   const dataUser = {
