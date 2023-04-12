e64376c7ed8540a0991fce7c3d78d028a2859706
Den4icGuti <42042543+Den4icGuti@users.noreply.github.com>
2023-04-12 17:21:16 +0300

    Update service.js

--- js/service.js
+++ js/service.js
@@ -1,15 +1,15 @@
-import Toastify from "toastify-js";
-import "../node_modules/toastify-js/src/toastify.css";
+// import Toastify from "toastify-js";
+// import "../node_modules/toastify-js/src/toastify.css";
 
-export const toast = Toastify({
-  text: "All fields must be hidden",
-  duration: 3000,
-  newWindow: true,
-  close: true,
-  gravity: "top", // `top` or `bottom`
-  position: "center", // `left`, `center` or `right`
-  stopOnFocus: true, // Prevents dismissing of toast on hover
-  style: {
-    background: "#b1693d",
-  },
-});
+// export const toast = Toastify({
+//   text: "All fields must be hidden",
+//   duration: 3000,
+//   newWindow: true,
+//   close: true,
+//   gravity: "top", // `top` or `bottom`
+//   position: "center", // `left`, `center` or `right`
+//   stopOnFocus: true, // Prevents dismissing of toast on hover
+//   style: {
+//     background: "#b1693d",
+//   },
+// });
