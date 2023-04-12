c1562110611b90572e1ad60acbee44ba006e7c11
Den4icGuti <goos7412@gmail.com>
2023-04-12 16:15:17 +0300

    add toast

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-import { toast } from "../js/service";
+import { toast } from "./service.js";
 const refs = {
   btnOpenForm: document.querySelector(".js-btn-form"),
   formGroup: document.querySelector(".js-form-group"),
