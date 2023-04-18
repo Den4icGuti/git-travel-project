8addeee8302d6e21cebeb039c0cbeed7f1a68a68
Den4icGuti <goos7412@gmail.com>
2023-04-18 19:19:01 +0300

    add section visit, add animation

--- js/index.js
+++ js/index.js
@@ -1,23 +1,51 @@
 // import { toast } from "./service";
+import Aos from "aos";
+
+import "../node_modules/aos/dist/aos.css";
+
+Aos.init();
+Aos.init({
+  // Global settings:
+  disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+  startEvent: "DOMContentLoaded", // name of the event dispatched on the document, that AOS should initialize on
+  initClassName: "aos-init", // class applied after initialization
+  animatedClassName: "aos-animate", // class applied on animation
+  useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+  disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+  debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+  throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+  // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+  offset: 120, // offset (in px) from the original trigger point
+  delay: 0, // values from 0 to 3000, with step 50ms
+  duration: 400, // values from 0 to 3000, with step 50ms
+  easing: "ease", // default easing for AOS animations
+  once: false, // whether animation should happen only once - while scrolling down
+  mirror: false, // whether elements should animate out while scrolling past them
+  anchorPlacement: "top-bottom", // defines which position of the element regarding to window should trigger the animation
+});
 const refs = {
   btnOpenForm: document.querySelector(".js-btn-form"),
   formGroup: document.querySelector(".js-form-group"),
   form: document.querySelector(".js-sign-form"),
+  animateScroll: document.querySelector(".js-anim_items"),
 };
+
+const { animateScroll, btnOpenForm, formGroup, form } = refs;
 
 console.log(refs);
 
-refs.btnOpenForm.addEventListener("click", onOpenForm);
+btnOpenForm.addEventListener("click", onOpenForm);
 
-refs.formGroup.addEventListener("click", onBackdropClick);
+formGroup.addEventListener("click", onBackdropClick);
 
 function onOpenForm() {
-  refs.formGroup.classList.add("show-form");
+  formGroup.classList.add("show-form");
   window.addEventListener("keydown", onKeyPressEsc);
 }
 
 function onCloseForm() {
-  refs.formGroup.classList.remove("show-form");
+  formGroup.classList.remove("show-form");
   window.removeEventListener("keydown", onKeyPressEsc);
 }
 
@@ -50,8 +78,8 @@ function onSubmitForm(e) {
   };
 
   const newUser = [dataUser, ...db];
-
-  refs.form.reset();
+  console.log(newUser);
+  form.reset();
 }
 
-refs.form.addEventListener("submit", onSubmitForm);
+form.addEventListener("submit", onSubmitForm);
